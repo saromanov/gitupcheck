0039277c20e61a823916424f9817ce78a18dc407
Sergey Romanov <xxsmotur@gmail.com>
2015-05-03 22:23:21 +0500

    Append redis backend

--- README.md
+++ README.md
@@ -3,6 +3,18 @@
 
 #Usage
 
+Append new item
+```sh
+python3.4 gitupcheck.py --path ../forked/foobar --remotepath https://github.com/saromanov/gitupcheck/ --store ../githublist
+```
 
+Append new item to redis
+```sh
+python3.4 gitupcheck.py --path ../forked/foobar --remotepath https://github.com/saromanov/gitupcheck/ --store @redis --addr localhost:6379
+```
+
+Get changes
+
+```sh
 python3.4 gitupcheck.py --check ../githublist
-python3.4 gitupcheck.py --path ../forked/foobar --remotepath fun --store ../githublist
+```
--- gitupcheck.py
+++ gitupcheck.py
@@ -3,16 +3,21 @@ import argparse
 import logging
 import os
 
+logging.basicConfig(format=u'%(filename)s[LINE:%(lineno)d]# %(levelname)-8s [%(asctime)s]  %(message)s')
 
-#Usage without git module
-#http://stackoverflow.com/questions/9774972/trying-to-execute-git-command-using-python-script
 
-#python3.4 gitupcheck.py --check ../githublist
-#python3.4 gitupcheck.py --path ../forked/celery --remotepath fun --store ../githublist
+REDISNAME = '@redis'
+DBNAME = 'gitupcheck'
 
 class GitUpCheck:
-	def __init__(self, store=None):
+	def __init__(self, store=None, addr='localhost:6379'):
+		""" store - path to local file with format
+			'remote path' 'local path'
+
+			addr - address to remote store. Now supported is redis and in that case, addr can be 'localhost:6379'
+		"""  
 		self.store = store
+		self.addr = addr
 
 	def _getFromFile(self, path):
 		''' Get infrormation about local and remote repos
@@ -26,14 +31,43 @@ class GitUpCheck:
 			yield remotepath, path
 		f.close()
 
+	def _getData(self, store):
+		""" Get data from local file or from remote store """
+		if store == REDISNAME:
+			checker = self._getFromRedis()
+			result = list(map(lambda x: str(x).split(':'), self._getFromRedis()))
+			return [(value[0][1:], value[1][:-1]) for value in result]
+		else:
+			return self._getFromFile(store)
+
 	def _appendToFile(self, path, data):
 		''' Write new item to store '''
+		log.info("Start read from file")
 		f = open(path, 'a')
 		f.write('\n')
 		f.write(data)
 		f.close()
+		log.info("Complete read from file")
 
-	def addItem(self, path, remotepath, store=None):
+	def _prepareAddress(self):
+		value = self.addr.split(':')
+		host = value[0]
+		port = value[1]
+		return host, port
+
+	def _appendToRedis(self, data):
+		import redis
+		host, port = self._prepareAddress()
+		client = redis.Redis(host=host, port=port)
+		client.lpush("gitupcheck", data)
+
+	def _getFromRedis(self):
+		import redis
+		logging.info("Getting data from redis")
+		host, port = self._prepareAddress()
+		return redis.Redis(host=host, port=port).lrange('gitupcheck', 0, -1)
+
+	def addItem(self, path, remotepath, store=None, addr=None):
 		'''	Add and check new item
 		store - path to file with information about local and remore repos
 		path - local path to forked repository for example: ~/gitupcheck
@@ -44,9 +78,13 @@ class GitUpCheck:
 			logging.debug(msg)
 			raise Exception(msg)
 		check = Git(path)
-		check.execute(["git", "remote", "add", "upstream", remotepath])
+		print(check.execute(["git", "remote", "add", "upstream", remotepath]))
 		check.execute(["git", "fetch", "upstream"])
-		self._appendToFile(store, remotepath+ ' ' + path)
+		if store == '@redis':
+			self.addr = addr
+			self._appendToRedis(remotepath+ ':' + path)
+		else:
+			self._appendToFile(store, remotepath+ ' ' + path)
 		print("Item was append: ")
 		self._get_changes(path, gitclient=check)
 
@@ -64,7 +102,9 @@ class GitUpCheck:
 	def run(self):
 		''' Start checking changes on repos
 		'''
-		repos = self._getFromFile(self.store)
+		repos = self._getData(self.store)
+		logging.debug("Getting data from store: ")
+		logging.debug(repos)
 		for remotepath, path in repos: 
 			print("Getting changes from {0} to {1}".format(remotepath, path))
 			self._get_changes(path)
@@ -75,9 +115,11 @@ def main(results):
 	remotepath = results.remotepath
 	checker = GitUpCheck()
 	if path != None and remotepath != None and results.store != None:
-		checker.addItem(path, remotepath, store=results.store)
+		checker.addItem(path, remotepath, store=results.store, addr=results.store)
 	if results.check != None:
 		checker.store = results.check
+		if results.addr != None:
+			checker.addr = results.addr
 		checker.run()
 	else:
 		logging.error("Error in parsing arguments")
@@ -86,6 +128,7 @@ def parsing(parse):
 	parse.add_argument("--path", help="Local path to repository")
 	parse.add_argument("--remotepath", help="Remote path to repository")
 	parse.add_argument("--store", help="Path to store with data")
+	parse.add_argument("--addr", help="Append address host:port to remote store")
 	parse.add_argument("--check", help="Check current list. Argument path to list")
 	main(parse.parse_args())
 
